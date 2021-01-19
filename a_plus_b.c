42903655fe5fd1a7c799a58a2047ed462f27e3a0
steambird1 <77623231+steambird1@users.noreply.github.com>
2021-01-19 07:32:10 +0000

    a plus b
    calculating a+b by inputting a and b.

---
+++ a_plus_b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define H int main() {
+#define e int a,b,s=0;
+#define l if (s==2) return 0;
+#define o if (s==0) scanf("%d%d",&a,&b); \
+else printf("%d\n",a+b);
+#define w s=1;
+#define r s=2;
+#define d }
+
+H e l l o  w o r l d 
